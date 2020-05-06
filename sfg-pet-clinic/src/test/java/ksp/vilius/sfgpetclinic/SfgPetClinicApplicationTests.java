bae549224ea9a2763dee367b2fc0bafed80b574a
vilksp <v.kasperavicius1@gmail.com>
2020-05-06 15:55:46 +0300

    spring starting project

---
+++ sfg-pet-clinic/src/test/java/ksp/vilius/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ksp.vilius.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
