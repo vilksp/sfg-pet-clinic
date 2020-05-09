7b2279ddb567c4e0de4bdcb52594f47656463890
vilksp <v.kasperavicius1@gmail.com>
2020-05-09 23:50:46 +0300

    refactoring services

--- sfg-pet-clinic/pet-clinic-data/src/main/java/ksp/vilius/sfgpetclinic/services/VetService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/ksp/vilius/sfgpetclinic/services/VetService.java
@@ -2,12 +2,6 @@ package ksp.vilius.sfgpetclinic.services;
 
 import ksp.vilius.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
