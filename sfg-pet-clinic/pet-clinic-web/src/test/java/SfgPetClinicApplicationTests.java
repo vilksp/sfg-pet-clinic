e412c82cd2e47e762df0e731a099cb0be33c3d32
vilksp <v.kasperavicius1@gmail.com>
2020-05-09 22:29:55 +0300

    maven module error fixes

---
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
@@ -0,0 +1,12 @@
+import ksp.vilius.petclinic.SfgPetClinicApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SfgPetClinicApplication.class)
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
+
